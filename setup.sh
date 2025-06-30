a3c00fe2debf57a38387262db978417989e70ce0
yassinedkk <118664130+yassinedkk@users.noreply.github.com>
2025-06-30 20:14:48 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Installe R et les dépendances système
-sudo apt-get update
-sudo apt-get install -y r-base libcurl4-openssl-dev libssl-dev libxml2-dev
+# setup.sh - Version compatible Render
+apt-get update -qq
+apt-get install -y --no-install-recommends \
+    r-base \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libxml2-dev
