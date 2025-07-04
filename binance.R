c0b12e8877c5a013a9b3ff1230f7aaed310adedb
yassinedkk <118664130+yassinedkk@users.noreply.github.com>
2025-07-04 20:57:42 +0200

    binance.R

--- binance.R
+++ binance.R
@@ -7,7 +7,7 @@ library(readr)
 # Paramètres
 SYMBOL <- "BTCUSDT"
 fetch_trades <- function(symbol, start_time, end_time, limit = 1000) {
-  proxy_base <- "https://binance-proxy.remarkt-binance.workers.dev"
+  proxy_base <- "https://binance-proxy-fly-quiet-bush-9751.fly.dev"
 
   path <- "/api/v3/aggTrades"
   query <- list(
