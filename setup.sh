f952f729d69d194687d217abff91b7ff013f4476
yassinedkk <118664130+yassinedkk@users.noreply.github.com>
2025-07-06 23:04:26 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup.sh - Version compatible Render
-apt-get update -qq
-apt-get install -y --no-install-recommends \
-    r-base \
-    libcurl4-openssl-dev \
-    libssl-dev \
-    libxml2-dev
